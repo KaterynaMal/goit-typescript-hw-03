5288432f658bf02723c61ef6f19e74ec8bdb1c77
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-02-13 23:29:45 +0200

    task 4

--- dist/4.js
+++ dist/4.js
@@ -1,22 +1,53 @@
 class Key {
-    constructor(parameters) {
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature() {
+        return this.signature;
     }
 }
-;
 class Person {
-    constructor(parameters) {
+    constructor(key) {
+        this.key = key;
+    }
+    getKey() {
+        return this.key;
     }
 }
-;
 class House {
-    constructor(parameters) {
+    constructor(key) {
+        this.door = false;
+        this.key = key;
+        this.tenants = [];
+    }
+    comeIn(person) {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`Person ${person.getKey().getSignature()} entered the house.`);
+        }
+        else {
+            console.log("The door is closed.");
+        }
     }
 }
-;
-class MyHouse {
-    constructor(parameters) {
+class MyHouse extends House {
+    constructor(key) {
+        super(key);
+    }
+    openDoor(key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("The door is opened.");
+        }
+        else {
+            console.log("The key does not match.");
+        }
     }
 }
-;
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+house.openDoor(person.getKey());
+house.comeIn(person);
 export {};
 //# sourceMappingURL=4.js.map
--- src/4.ts
+++ src/4.ts
@@ -1,36 +1,70 @@
-
 class Key {
-    constructor(parameters) {
-        
-    }
-};
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
 class Person {
-    constructor(parameters) {
-        
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(
+        `Person ${person.getKey().getSignature()} entered the house.`
+      );
+    } else {
+      console.log("The door is closed.");
     }
-};
+  }
+  abstract openDoor(key: Key): void;
+}
 
-class House {
-    constructor(parameters) {
-        
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is opened.");
+    } else {
+      console.log("The key does not match.");
     }
-};
+  }
+}
 
-class MyHouse {
-    constructor(parameters) {
-        
-    }
-};
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
 
+house.openDoor(person.getKey());
 
-// const key = new Key();
-// const house = new MyHouse(key);
-// const person = new Person(key);
-
-// house.openDoor(person.getKey());
-
-// house.comeIn(person);
-
+house.comeIn(person);
 
 export {};
